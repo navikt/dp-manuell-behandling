5174b935a080e1d31e723ed9fd5dfee691e9da62
André Roaldseth <andre.roaldseth@nav.no>
2024-03-05 11:06:15 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("com.diffplug.spotless") version "6.25.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        showExceptions = true
+        showStackTraces = true
+        exceptionFormat = TestExceptionFormat.FULL
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+    }
+}
+
+configure<SpotlessExtension> {
+    kotlin {
+        ktlint()
+    }
+
+    kotlinGradle {
+        ktlint()
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    dependsOn("spotlessApply")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "dp-manuell-behandling"
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("no.nav.dagpenger:dp-version-catalog:20240304.70.b170f7")
+        }
+    }
+}
